735a04d9ae26ff00b943764f3ade510c651b5e2d
Marco Hao <hsm123@bupt.edu.cn>
2018-07-18 08:51:00 +0800

    ticket frontend

--- backend/ticketQuerySql.php
+++ backend/ticketQuerySql.php
@@ -7,7 +7,12 @@
 <head><title>Ticket Query Sql</title></head>
 
 <body>
-	
+	<?php
+		$from = $_POST['from'];
+		$to = $_POST['to'];
+		$departure = $_POST['departure'];
+		echo $from." ",$to." ",$departure." ";
+	?>
 </body>
 
 </html>
