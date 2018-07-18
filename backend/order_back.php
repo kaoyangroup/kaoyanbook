d98644f62591ff033c930ff05c49b44a04d5c244
zzzwj <30688153+Inexperience@users.noreply.github.com>
2018-07-19 01:57:26 +0800

    order相关

---
+++ backend/order_back.php
@@ -0,0 +1,102 @@
+<?php
+function update($orderId,$uid,$f)
+{
+	require_once './includes/dbh.inc.php';
+	//Order
+	$sql_order = "select * from Order1 where orderId='$orderId'";
+	$res_order = mysqli_query($con,$sql_order);
+	$row_order = mysqli_fetch_array($res_order);
+	$ticketId = $row_order["ticketId"];
+	$from = $row_order["from"];
+	$to = $row_order["to"];
+
+	//Myorder
+	$uid = $_COOKIE["cur_uid"];
+
+	//Booking
+	$sql_booking = "select * from Booking where ticketId='$ticketId'";
+	$res_booking = mysqli_query($con,$sql_booking);
+	$row_booking = mysqli_fetch_array($res_booking);
+	$lineId = $row_booking["lineId"];
+	$seatId = $row_booking["seatId"];
+	$marginCode = $row_booking["marginCode"];
+	$marginTicket = $row_booking["marginTicket"];
+	$goDate = $row_booking["date"];
+
+	//seat
+	$sql_seat = "select * from Seat where seatId='$seatId'";
+	$res_seat = mysqli_query($con,$sql_seat);
+	$row_seat = mysqli_fetch_array($res_seat);
+	$carriageId = $row_seat["carriageId"];
+	$seatNo = $row_seat["seatNo"];
+
+	//carriage
+	$sql_carriage = "select * from Carriage where carriageId='$carriageId'";
+	$res_carriage = mysqli_query($con,$sql_carriage);
+	$row_carriage = mysqli_fetch_array($res_carriage);
+	$trainId = $row_carriage["trainId"];
+	$carriageType = $row_carriage["carriageType"];
+	$carriageNo = $row_carriage["carriageNo"];
+
+	//train
+	$sql_train = "select * from Train where trainId='$trainId'";
+	$res_train = mysqli_query($con,$sql_train);
+	$row_train = mysqli_fetch_array($res_train);
+	$trainType = $row_train["trainType"];
+
+	//Line
+	$sql_line = "select * from Line where lineId='$lineId' and station='$from'";
+	$res_line = mysqli_query($con,$sql_line);
+	$row_line = mysqli_fetch_array($res_line);
+	$order_from = $row_line["arrOrder"];
+	$sql_line1 = "select * from Line where lineId='$lineId' and station='$to'";
+	$res_line1 = mysqli_query($con,$sql_line1);
+	$row_line1 = mysqli_fetch_array($res_line1);
+	$order_to = $row_line1["arrOrder"];
+
+	//Mileprice
+	$sql_price = "select * from Mileprice where start='$from' and end='$to' and lineType='$trainType' and seatType='$carriageType'";
+	$res_price = mysqli_query($con,$sql_price);
+	$row_price = mysqli_fetch_array($res_price);
+	$price = $row_price["price"];
+
+	$codeLength = strlen($marginCode);
+	for($i = $order_from;$i <= $order_to;$i++)
+	{
+		if($marginCode[$i] == '1')
+			$marginCode[$i] = '0';
+	}
+	//
+	$marginNum = 0;
+	for($j = 0;$j < $codeLength;$j++)
+	{
+		if($marginCode[$j] == '0')
+			$marginNum++;
+	}
+
+	//update booking
+	$sql_update_booking = "update Booking set marginCode='$marginCode',marginTicket='$marginNum' where ticketId='$ticketId'";
+	mysqli_query($con,$sql_update_booking);
+
+	//Margins
+	$sql_update_margins = "update Margins set marginTicket=marginTicket+1 where lineId='$lineId' and date='$goDate' and from='$from' and to='$to'";
+	mysqli_query($con,$sql_update_margins);
+
+	//update myorder
+	$sql_update_myorder = "delete from Myorder where orderId='$orderId'";
+	mysqli_query($con,$sql_update_myorder);
+
+	//update order
+	$sql_update_order = "delete from Order1 where orderId='$orderId'";
+	mysqli_query($con,$sql_update_order);
+
+	if($f == 1){
+		//update user
+		$sql_user = "update user set account=account+'$price' where uid='$uid'";
+		mysqli_query($con,$sql_user);
+	}
+	
+
+	// return array($from,$to,$trainId,$seatNo,$uid,$goDate,1);
+}
+?>
