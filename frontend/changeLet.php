2beea1eb619c1b873d79a50175af59ac1a335587
zzzwj <30688153+Inexperience@users.noreply.github.com>
2018-07-19 14:17:36 +0800

    changeLet.php 修改

--- frontend/changeLet.php
+++ frontend/changeLet.php
@@ -15,8 +15,17 @@ require_once '../backend/includes/dbh.inc.php';
 $sql = "select * from user where uid='$uid'";
 $result = mysqli_query($con,$sql);
 $row = mysqli_fetch_array($result);
+echo "<script type='text/javascript'>
+		function validate_email()
+		{
+			var re=/^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/;
+			if(re.test(cha.textfieldB.value))
+			{return true}
+			else{alert('邮箱格式不正确！');return false}
+		}	
+	</script>";
 echo "<p>原电子邮箱：&nbsp;&nbsp;&nbsp;&nbsp;".$row["email"]."</p>";
-echo "<form action='changeLetSQL.php' method='post'>";
+echo "<form name='cha' action='changeLetSQL.php' method='post' onsubmit='return validate_email()'>";
 echo "<p>新电子邮箱：<input type='text' name='textfieldB' /></p>";
 echo "<p>登录密码：&nbsp;&nbsp;&nbsp;&nbsp;<input type='password' name='textfieldC' /></p>";
 echo "<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;正确输入密码才能修改个人资料</p>";
